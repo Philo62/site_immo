f1b164a9387bb6fae01e56aefd36c892d6209a5b
Philo <Philo621@gmail.com>
2019-11-04 15:42:25 +0100

    fucking options

---
+++ .history/src/Form/PropertySearchType_20191104150432.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\PropertySearch;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PropertySearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('minSurface', IntegerType::class, [
+                'required' => false,
+                'label' => false,
+                'attr'=> [
+                    'placeholder' => 'Surface minimale'
+                ]
+            ])
+                ->add('maxPrice', IntegerType::class, [
+                    'required' => false,
+                    'label' => false,
+                    'attr'=> [
+                        'placeholder' => 'Budget Max'
+                    ]
+            ])
+                ->add('options',EntityType::class,[
+                    'required' => false,
+                    'label' => false,
+                    'class' => Option::class,
+                    'choice'
+                ])
+        ;                  
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => PropertySearch::class,
+            'method' => 'get',
+            'csrf_protection' => false
+        ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return ''; 
+    }
+}
---
+++ .history/src/Form/PropertyType_20191104143238.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Property;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use App\Entity\Option;
+
+class PropertyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('surface')
+            ->add('rooms')
+            ->add('bedrooms')
+            ->add('floor')
+            ->add('price')
+            ->add('heat', ChoiceType::class, [
+                'choices' => $this->getChoises()
+            ])
+            ->add('options', EntityTy::class,[
+                'class' => Option::class,
+                'choice_label' => 'name',
+                'multiple' =>true            
+            ])
+            ->add('city')
+            ->add('address')
+            ->add('postal_code')
+            ->add('sold')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+            'translation_domain' => 'forms'
+        ]);
+    }
+
+    private function getChoises()
+    {
+        $choices = Property::HEAT;
+        $output = [];
+        foreach($choices as $k => $v) {
+            $output[$v] = $k;
+        }
+        return $output; 
+    }
+}
