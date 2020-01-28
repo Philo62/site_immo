e84cfbfeb407c47aa549bcc73d12eb9de2d70954
Philo <philo621@gmail.com>
2020-01-28 17:57:37 +0100

    Css touch

--- .history/assets/js/app_20191119133519.js
+++ .history/assets/js/app_20191119133519.js
@@ -5,7 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 global.$ = global.jQuery = $ ;
-// any CSS you require will output into a single css file (app.css in this case)
+// // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
 require('select2')
 
